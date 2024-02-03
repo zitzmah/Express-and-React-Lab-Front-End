c421686584edc785418f94178962398abe2bf9de
HZ <haleyzitzmann@gmail.com>
2024-02-03 11:06:46 -0700

    updated router

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,21 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 
 function Projects(props) {
-        
-    const projects = useLoaderData()
+
+  const projects = useLoaderData()
 
     return projects.map((project) => (
-        <div>
-            <h1>{project.name}</h1>
-            <img src={project.image}/>
-            <a href={project.git}>
-                <button>Github</button>
-            </a>
-            <a href={project.live}>
-                <button>live site</button>
-            </a>
-        </div>
-    ))
+      <div>
+        <h1>{project.name}</h1>
+        <img src={project.image} />
+        <a href={project.git}>
+          <button>Github</button>
+        </a>
+        <a href={project.live}>
+          <button>live site</button>
+        </a>
+      </div>
+    ));
 }
 
 export default Projects;
--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom";
+import { createRoutesFromElements,createBrowserRouter, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -6,11 +6,11 @@ import Projects from "./pages/Projects";
 import { aboutLoader, projectsLoader } from "./loaders";
 
 const router = createBrowserRouter(createRoutesFromElements(
-    <Router path="/" element={<App/>}>
+    <Route path="/" element={<App/>}>
         <Route path="" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-    </Router>
+        <Route path="/about" element={<About/>} loader={aboutLoader}/>
+        <Route path="/projects" element={<Projects/>} loader={projectsLoader}/>
+    </Route>
 ))
 
 export default router
