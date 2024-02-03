fd1f87fa1ae669098b67b9bcf57184354c0588e9
HZ <haleyzitzmann@gmail.com>
2024-02-03 11:10:53 -0700

    index.js updated correctly

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <<RouterProvider router={router}/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
